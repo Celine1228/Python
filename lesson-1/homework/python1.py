2728d94af16c1fb564115e3673c1670e761ff752
Celine1228 <robiyaabdusamatova@gmail.com>
2025-07-10 10:05:31 +0500

    Create python1.py

---
+++ lesson-1/homework/python1.py
@@ -0,0 +1,26 @@
+#1
+side=float(input("Enter the side lenght of square"))
+perimeter=side*4
+area=side**2
+print("perimetr of the square:", perimeter)
+print("area of the square:", area)
+#2
+diameter=float(input("enter diameter"))
+length=diameter*3.14
+print("length of the square", length)
+#3
+number_one=input("enter first number")
+number_two=input("enter second number")
+mean = (float(number_one) + float(number_two)) / 2
+print("result", mean)
+#4
+a = float(input("Enter the first number (a): "))
+b = float(input("Enter the second number (b): "))
+sum_ab = a + b
+product_ab = a * b
+square_a = a ** 2
+square_b = b ** 2
+print("Sum:", sum_ab)
+print("Product:", product_ab)
+print("Square of a:", square_a)
+print("Square of b:", square_b)
